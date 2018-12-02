47c37338902e7c13611c9afcf3ecdb81adac97ac
shabirshad <shabirshad@gmail.com>
2018-12-02 15:53:11 -0500

    Test totalSupply

---
+++ contracts/DappToken.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.2;
+
+contract DappToken {
+	//constructor 
+	//Set token supply
+	//read token supply
+
+	uint256 public totalSupply;
+
+	function DappToken () public {
+	totalSupply = 1000000;
+	
+	}
+}
